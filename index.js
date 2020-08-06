d9b4c40738d22da09abff653a75780071e99aeba
Megan Horsfield <meganthadev@gmail.com>
2020-08-06 00:47:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ function map(source, func) {
   return r;
 }
 
-function reduce(source, func) {
-  let 
+function reduce(source, func, starting) {
+  let r = 
 }
+
+function reduce(src, cb, starting) {
+  let r = (!!starting) ? starting : src[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < src.length; i++) {
+    r = cb(src[i], r)
+  }
+
+  return r;
+} 
