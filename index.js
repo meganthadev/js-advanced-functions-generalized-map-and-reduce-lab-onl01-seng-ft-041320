a4c0076db35a679856572578f1acbba94301a3ec
Megan Horsfield <meganthadev@gmail.com>
2020-08-06 00:50:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your functions here
+function map(source, func) {
+  let r = []
+  for (let i = 0; i < source.length; i++) {
+    let element = source[i]
+    r.push(func(element))
+  }
+
+  return r;
+}
+
+function reduce(source, func, starting){
+  let r = (!!starting) ? starting : source[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < source.length; i++) {
+    r = func(source[i], r)
+  }
+
+  return r;
+}
