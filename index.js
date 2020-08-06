129110fddb4e3ee0d03c3fafb42e2ca7522ba595
Megan Horsfield <meganthadev@gmail.com>
2020-08-06 00:44:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function map(source, func) {
   let r = []
-  for (let i = 0; i < src.length; i++) {
+  for (let i = 0; i < source.length; i++) {
     let element = source[i]
     r.push(func(element))
   }
