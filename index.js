d7bdbc98d39f5bb1e5843dcb2056b164eca6d018
Megan Horsfield <meganthadev@gmail.com>
2020-08-06 00:43:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Add your functions here
+function map(source, func) {
+  let r = []
+  for (let i = 0; i < src.length; i++) {
+    let element = source[i]
+    r.push(cb(element))
+  }
+
+  return r;
+}
